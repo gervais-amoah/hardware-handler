fed7164205ce7d729785c996e3c799b2ff067068
Gervais Amoah <gervaisamoah@yahoo.com>
2023-11-22 22:06:17 +0000

    remove unused imports from `ProductForm.js` file

--- client/src/containers/ProductForm/ProductForm.js
+++ client/src/containers/ProductForm/ProductForm.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import Loader from "../../components/Loader/Loader";
 import {
