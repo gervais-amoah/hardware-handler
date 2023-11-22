52e7ed0635526785d5a3d35d504fe6bff333ad5b
Gervais Amoah <gervaisamoah@yahoo.com>
2023-11-22 23:36:20 +0000

    Refactor ProductList.js

--- client/src/containers/ProductList/ProductList.js
+++ client/src/containers/ProductList/ProductList.js
@@ -1,206 +1,194 @@
-import React, { Component } from 'react';
-import { toast } from 'react-toastify';
-import Product from '../../components/Product/Product';
-import Loader from '../../components/Loader/Loader';
-import { formatFilters } from '../../helpers/formatFilters';
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader/Loader";
+import Product from "../../components/Product/Product";
 import {
-  PRODUCT_ADDED_TO_CHECKOUT_SUCCESS,
   FETCH_DEPARTMENT_DATA_ERROR,
   FETCH_PRODUCT_DATA_ERROR,
   MULTIPLE_ERRORS,
-} from '../../constants/constants';
-import * as productApi from '../../services/productApi';
-import * as checkoutApi from '../../services/checkoutApi';
-import * as departmentApi from '../../services/departmentApi';
-import './ProductList.css';
+  PRODUCT_ADDED_TO_CHECKOUT_SUCCESS,
+} from "../../constants/constants";
+import { formatFilters } from "../../helpers/formatFilters";
+import * as checkoutApi from "../../services/checkoutApi";
+import * as departmentApi from "../../services/departmentApi";
+import * as productApi from "../../services/productApi";
+import "./ProductList.css";
 
-class ProductList extends Component {
-  constructor(props) {
-    super(props);
+function ProductList({ updateCheckoutCount }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [departmentsError, setDepartmentsError] = useState(false);
+  const [productsError, setProductsError] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [filtersByBrand, setFiltersByBrand] = useState([]);
+  const [filtersByDepartment, setFiltersByDepartment] = useState([]);
+  const [activeFilter, setActiveFilter] = useState([]);
 
-    this.state = {
-      products: [],
-      loading: true,
-      error: false,
-      errMsg: '',
-      filtersByBrand: [],
-      filtersByDepartment: [],
-      activeFilter: [],
-    };
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const products = await productApi.getAllProducts();
+      const filtersByDepartment = await departmentApi.getAllDepartments();
 
-  async componentDidMount() {
-    const products = await productApi.getAllProducts();
-    const filtersByDepartment = await departmentApi.getAllDepartments();
-
-    if (
-      products !== FETCH_PRODUCT_DATA_ERROR &&
-      filtersByDepartment !== FETCH_DEPARTMENT_DATA_ERROR
-    ) {
-      const filtersByBrand = formatFilters(products, 'brand').sort((a, b) => {
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-      });
-
-      this.setState({
-        products,
-        loading: false,
-        error: false,
-        filtersByBrand,
-        filtersByDepartment,
-      });
-    } else {
-      if (products === FETCH_PRODUCT_DATA_ERROR) {
-        this.setState({
-          loading: false,
-          error: true,
-          errMsg: products,
-        });
-      }
-      if (filtersByDepartment === FETCH_DEPARTMENT_DATA_ERROR) {
-        this.setState({
-          loading: false,
-          error: true,
-          errMsg: filtersByDepartment,
-        });
-      }
       if (
-        products === FETCH_PRODUCT_DATA_ERROR &&
-        filtersByDepartment === FETCH_DEPARTMENT_DATA_ERROR
+        products !== FETCH_PRODUCT_DATA_ERROR &&
+        filtersByDepartment !== FETCH_DEPARTMENT_DATA_ERROR
       ) {
-        this.setState({
-          loading: false,
-          error: true,
-          errMsg: MULTIPLE_ERRORS,
+        const filtersByBrand = formatFilters(products, "brand").sort((a, b) => {
+          const nameA = a.name.toUpperCase();
+          const nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
         });
-      }
-    }
-  }
 
-  addItemToCheckout = async (product) => {
+        setProducts(products);
+        setFiltersByBrand(filtersByBrand);
+        setFiltersByDepartment(filtersByDepartment);
+        setProductsError(false);
+        setDepartmentsError(false);
+      } else {
+        if (products === FETCH_PRODUCT_DATA_ERROR) {
+          setErrMsg(products);
+          setProductsError(true);
+        }
+        if (filtersByDepartment === FETCH_DEPARTMENT_DATA_ERROR) {
+          setErrMsg(filtersByDepartment);
+          setDepartmentsError(true);
+        }
+        if (
+          products === FETCH_PRODUCT_DATA_ERROR &&
+          filtersByDepartment === FETCH_DEPARTMENT_DATA_ERROR
+        ) {
+          setErrMsg(MULTIPLE_ERRORS);
+          setProductsError(true);
+          setDepartmentsError(true);
+        }
+      }
+
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  async function addItemToCheckout(product) {
     const productAdded = await checkoutApi.addItemToCheckout(product);
     if (productAdded === PRODUCT_ADDED_TO_CHECKOUT_SUCCESS) {
-      this.setState({
-        loading: false,
-        error: false,
-      });
-      this.props.updateCheckoutCount();
+      updateCheckoutCount();
       toast.success(PRODUCT_ADDED_TO_CHECKOUT_SUCCESS);
     } else {
-      this.setState({
-        error: false,
-        loading: false,
-      });
       toast.error(productAdded);
     }
-  };
 
-  onFilterChange(filter) {
-    if (this.state.activeFilter.includes(filter)) {
-      const filterIndex = this.state.activeFilter.indexOf(filter);
-      const newFilter = [...this.state.activeFilter];
+    setLoading(false);
+  }
+
+  function onFilterChange(filter) {
+    if (activeFilter.includes(filter)) {
+      const filterIndex = activeFilter.indexOf(filter);
+      const newFilter = [...activeFilter];
       newFilter.splice(filterIndex, 1);
-      this.setState({ activeFilter: newFilter });
+      setActiveFilter(newFilter);
     } else {
-      this.setState({ activeFilter: [...this.state.activeFilter, filter] });
+      setActiveFilter([...activeFilter, filter]);
     }
   }
 
-  render() {
-    let filteredList;
-    const { loading, error, errMsg } = this.state;
+  let filteredList;
 
-    if (
-      this.state.activeFilter.length === 0 ||
-      this.state.activeFilter.length ===
-        this.state.filtersByBrand.length + this.state.filtersByDepartment.length
-    ) {
-      filteredList = this.state.products;
-    } else {
-      filteredList = this.state.products.filter((item) => {
-        return (
-          this.state.activeFilter.includes(item.brand) ||
-          this.state.activeFilter.includes(item.departmentId)
-        );
-      });
-    }
+  if (
+    activeFilter.length === 0 ||
+    activeFilter.length === filtersByBrand.length + filtersByDepartment.length
+  ) {
+    filteredList = products;
+  } else {
+    filteredList = products.filter((item) => {
+      return (
+        activeFilter.includes(item.brand) ||
+        activeFilter.includes(item.departmentId)
+      );
+    });
+  }
 
-    return (
-      <div className="product-list-container">
-        <section className="filter-wrapper">
-          <p className="filter-title">Filter by Department</p>
-          <div className="filter-data">
-            {error ? <p>Cannot load department filters.</p> : null}
-            {!error && this.state.filtersByDepartment.length
-              ? this.state.filtersByDepartment.map((filter) => (
-                  <span key={filter.id} className="filter-item">
-                    <label htmlFor={filter.id}>{filter.name}</label>
-                    <input
-                      className="filter-checkbox"
-                      id={filter.id}
-                      type="checkbox"
-                      checked={this.state.activeFilter.includes(filter.id)}
-                      onChange={() => this.onFilterChange(filter.id)}
-                    />
-                  </span>
-                ))
-              : null}
-          </div>
-          <p className="filter-title">Filter by Brand</p>
-          <div className="filter-data">
-            {error ? <p>Cannot load product brand filters.</p> : null}
-            {!error && this.state.filtersByBrand.length
-              ? this.state.filtersByBrand.map((filter, index) => (
-                  <span key={index} className="filter-item">
-                    <label htmlFor={index}>{filter.name}</label>
-                    <input
-                      className="filter-checkbox"
-                      id={index}
-                      type="checkbox"
-                      checked={this.state.activeFilter.includes(filter.value)}
-                      onChange={() => this.onFilterChange(filter.value)}
-                    />
-                  </span>
-                ))
-              : null}
-          </div>
-        </section>
-        <h1 className="product-list-header">My Products</h1>
-        <section className="products-container">
-          {error ? (
+  return (
+    <div className="product-list-container">
+      <section className="filter-wrapper">
+        <p className="filter-title">Filter by Department</p>
+        <div className="filter-data">
+          {departmentsError ? (
+            <p>Cannot load department filters.</p>
+          ) : (
+            filtersByDepartment.length &&
+            filtersByDepartment.map((filter) => (
+              <span key={filter.id} className="filter-item">
+                <label htmlFor={filter.id}>{filter.name}</label>
+                <input
+                  className="filter-checkbox"
+                  id={filter.id}
+                  type="checkbox"
+                  checked={activeFilter.includes(filter.id)}
+                  onChange={() => onFilterChange(filter.id)}
+                />
+              </span>
+            ))
+          )}
+        </div>
+        <p className="filter-title">Filter by Brand</p>
+        <div className="filter-data">
+          {productsError ? (
+            <p>Cannot load product brand filters.</p>
+          ) : (
+            filtersByBrand.length &&
+            filtersByBrand.map((filter, index) => (
+              <span key={index} className="filter-item">
+                <label htmlFor={index}>{filter.name}</label>
+                <input
+                  className="filter-checkbox"
+                  id={index}
+                  type="checkbox"
+                  checked={activeFilter.includes(filter.value)}
+                  onChange={() => onFilterChange(filter.value)}
+                />
+              </span>
+            ))
+          )}
+        </div>
+      </section>
+      <h1 className="product-list-header">My Products</h1>
+      <section className="products-container">
+        {productsError || departmentsError ? (
+          <p className="product-list-message">
+            {errMsg} Please refresh the page or try again later.
+          </p>
+        ) : null}
+        {loading ? <Loader message="Loading product list..." /> : null}
+        <div className="product-list-product-wrapper">
+          {!loading && !productsError && filteredList.length
+            ? filteredList.map((product) => (
+                <Product
+                  key={product.id}
+                  product={product}
+                  addItemToCheckout={addItemToCheckout}
+                />
+              ))
+            : null}
+          {!loading &&
+          !productsError &&
+          !departmentsError &&
+          !filteredList.length ? (
             <p className="product-list-message">
-              {errMsg} Please refresh the page or try again later.
+              There are no products that match your filters. Please clear some
+              filters to see more producs.
             </p>
           ) : null}
-          {loading ? <Loader message="Loading product list..." /> : null}
-          <div className="product-list-product-wrapper">
-            {!loading && !error && filteredList.length
-              ? filteredList.map((product) => (
-                  <Product
-                    key={product.id}
-                    product={product}
-                    addItemToCheckout={this.addItemToCheckout}
-                  />
-                ))
-              : null}
-            {!loading && !error && !filteredList.length ? (
-              <p className="product-list-message">
-                There are no products that match your filters. Please clear some
-                filters to see more producs.
-              </p>
-            ) : null}
-          </div>
-        </section>
-      </div>
-    );
-  }
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default ProductList;
