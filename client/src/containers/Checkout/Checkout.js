2273e4075d376a8e408e5e0a9686ce465cd9ba1a
Gervais Amoah <gervaisamoah@yahoo.com>
2023-11-22 21:36:28 +0000

    Refactor Checkout.js

--- client/src/containers/Checkout/Checkout.js
+++ client/src/containers/Checkout/Checkout.js
@@ -1,100 +1,95 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
-import Loader from '../../components/Loader/Loader';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
+import Loader from "../../components/Loader/Loader";
 import {
   FETCH_CHECKOUT_PRODUCTS_ERROR,
-  REMOVE_PRODUCT_FROM_CHECKOUT_ERROR,
   PRODUCT_REMOVED_FROM_CHECKOUT_SUCCESS,
-} from '../../constants/constants';
-import * as checkoutApi from '../../services/checkoutApi';
-import './Checkout.css';
+  REMOVE_PRODUCT_FROM_CHECKOUT_ERROR,
+} from "../../constants/constants";
+import * as checkoutApi from "../../services/checkoutApi";
+import "./Checkout.css";
 
-class Checkout extends Component {
-  constructor(props) {
-    super(props);
+function Checkout({ updateCheckoutCount }) {
+  const [checkoutItems, setCheckoutItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    this.state = {
-      checkoutItems: [],
-      loading: true,
-      error: false,
-    };
-  }
+  useEffect(() => {
+    async function fetchCheckoutItems() {
+      const allCheckoutItems = await checkoutApi.getAllCheckoutItems();
 
-  async componentDidMount() {
-    const checkoutItems = await checkoutApi.getAllCheckoutItems();
-    if (checkoutItems !== FETCH_CHECKOUT_PRODUCTS_ERROR) {
-      this.setState({ checkoutItems, loading: false, error: false });
-    } else {
-      this.setState({ loading: false, error: true });
+      if (allCheckoutItems !== FETCH_CHECKOUT_PRODUCTS_ERROR) {
+        setCheckoutItems(allCheckoutItems);
+        setError(false);
+      } else {
+        setError(true);
+      }
+
+      setLoading(false);
     }
-  }
 
-  removeItemFromCheckout = async (id) => {
+    fetchCheckoutItems();
+  }, []);
+
+  async function removeItemFromCheckout(id) {
     const remainingCheckoutItems = await checkoutApi.removeProductFromCheckout(
-      id,
+      id
     );
+
     if (remainingCheckoutItems !== REMOVE_PRODUCT_FROM_CHECKOUT_ERROR) {
-      this.setState({
-        checkoutItems: remainingCheckoutItems,
-        loading: false,
-        error: false,
-      });
-      this.props.updateCheckoutCount();
+      setCheckoutItems(remainingCheckoutItems);
+      updateCheckoutCount();
       toast.success(PRODUCT_REMOVED_FROM_CHECKOUT_SUCCESS);
     } else {
       toast.error(remainingCheckoutItems);
-      this.setState({ loading: false, error: true });
     }
-  };
 
-  render() {
-    const { checkoutItems, loading, error } = this.state;
-    const { removeItemFromCheckout } = this;
-
-    return (
-      <div>
-        <h1 className="checkout-title">Checkout Page</h1>
-        <div>
-          {loading ? <Loader message="Fetching items to checkout..." /> : null}
-          {error ? (
-            <p className="checkout-message">
-              {FETCH_CHECKOUT_PRODUCTS_ERROR} Please refresh the page or try
-              again later.
-            </p>
-          ) : null}
-          {!loading & !error && checkoutItems.length ? (
-            <div>
-              <div className="checkout-header">
-                <div>Product Information</div>
-                <div>Suggested Retail Price</div>
-                <div>Update Checkout</div>
-              </div>
-              <ul className="checkout-list-wrapper">
-                {checkoutItems.map((item) => (
-                  <CheckoutItem
-                    key={item.id}
-                    item={item}
-                    removeItemFromCheckout={removeItemFromCheckout}
-                  />
-                ))}
-              </ul>
-            </div>
-          ) : null}
-          {!loading && !error && !checkoutItems.length ? (
-            <p className="checkout-message">
-              The checkout is currently empty. Add some items from the&nbsp;
-              <NavLink className="page-link" to="/my-products">
-                My Products
-              </NavLink>
-              &nbsp;page.
-            </p>
-          ) : null}
-        </div>
-      </div>
-    );
+    setLoading(false);
   }
+
+  return (
+    <div>
+      <h1 className="checkout-title">Checkout Page</h1>
+      <div>
+        {loading && <Loader message="Fetching items to checkout..." />}
+        {error && (
+          <p className="checkout-message">
+            {FETCH_CHECKOUT_PRODUCTS_ERROR} Please refresh the page or try again
+            later.
+          </p>
+        )}
+        {!loading & !error && checkoutItems.length && (
+          <div>
+            <div className="checkout-header">
+              <div>Product Information</div>
+              <div>Suggested Retail Price</div>
+              <div>Update Checkout</div>
+            </div>
+            <ul className="checkout-list-wrapper">
+              {checkoutItems.map((item) => (
+                <CheckoutItem
+                  key={item.id}
+                  item={item}
+                  removeItemFromCheckout={removeItemFromCheckout}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+        {!loading && !error && !checkoutItems.length && (
+          <p className="checkout-message">
+            The checkout is currently empty. Add some items from the&nbsp;
+            <NavLink className="page-link" to="/my-products">
+              My Products
+            </NavLink>
+            &nbsp;page.
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
