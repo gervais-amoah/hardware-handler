40d1bdccf162964f157cf8baffcf262c79d29a30
Gervais Amoah <gervaisamoah@yahoo.com>
2023-11-25 18:29:57 +0000

    refactor `useCheckout` for `App.js` usecase

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -2,38 +2,26 @@ import React, { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import * as checkoutApi from "../../services/checkoutApi";
 import Checkout from "../Checkout/Checkout";
 import Home from "../Home/Home";
 import Navbar from "../Navbar/Navbar";
 import ProductForm from "../ProductForm/ProductForm";
 import ProductList from "../ProductList/ProductList";
 import "./App.css";
-import { FETCH_CHECKOUT_COUNT_ERROR } from "../../constants/constants";
+// import { FETCH_CHECKOUT_COUNT_ERROR } from "../../constants/constants";
+import { useCheckout } from "../../hooks/useCheckout";
 
 function App() {
-  const [checkoutCount, setCheckoutCount] = useState(0);
-  const [cartUpdated, setCartUpdated] = useState(false);
+  const [checkoutUpdated, setCheckoutUpdated] = useState(false);
+
+  const { checkoutCount } = useCheckout(checkoutUpdated);
 
   useEffect(() => {
-    async function fetchCheckoutItems() {
-      const checkoutItemsCount = await checkoutApi.getCheckoutCount();
-
-      if (
-        Number(checkoutItemsCount) ||
-        checkoutItemsCount === 0 ||
-        checkoutItemsCount === FETCH_CHECKOUT_COUNT_ERROR
-      ) {
-        setCheckoutCount(checkoutItemsCount);
-        setCartUpdated(false);
-      }
-    }
-
-    fetchCheckoutItems();
-  }, [cartUpdated]);
+    if (checkoutCount) setCheckoutUpdated(false);
+  }, [checkoutCount]);
 
   async function updateCheckoutCount() {
-    setCartUpdated(true);
+    setCheckoutUpdated(true);
   }
 
   return (
--- client/src/hooks/useCheckout.js
+++ client/src/hooks/useCheckout.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { getAllCheckoutItems } from "../services/checkoutApi";
 import { FETCH_CHECKOUT_PRODUCTS_ERROR } from "../constants/constants";
 
-export function useCheckout() {
+export function useCheckout(checkoutUpdated) {
   const [checkoutItems, setCheckoutItems] = useState([]);
+  const [checkoutCount, setCheckoutCount] = useState(0);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -18,12 +19,17 @@ export function useCheckout() {
     }
 
     fetchCheckoutItems();
-  }, []);
+  }, [checkoutUpdated]);
+
+  useEffect(() => {
+    const count = checkoutItems.length;
+    if (Number(count) || count === 0) setCheckoutCount(count);
+  }, [checkoutItems]);
 
   return {
-    checkoutItems,
-    setCheckoutItems,
-    checkoutCount: checkoutItems.length,
     error,
+    checkoutItems,
+    checkoutCount,
+    setCheckoutItems,
   };
 }
