18b68f11f8afaa7b9571e2062b0524245d239200
Gervais Amoah <gervaisamoah@yahoo.com>
2023-11-27 15:20:20 +0000

    Add testing packages and run first test

--- client/src/containers/App/test/App.test.js
+++ client/src/containers/App/test/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-test('renders learn react link', () => {
+test("renders Hardware Handler without crashing", () => {
   render(<App />);
   const title = screen.getByText(/welcome to hardware handler/i);
   expect(title).toBeInTheDocument();
