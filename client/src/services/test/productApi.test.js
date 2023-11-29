b171215e2c56148b997e6d7d068f3012d95181bb
Gervais Amoah <gervaisamoah@yahoo.com>
2023-11-29 22:30:40 +0000

    Delete spaces in productApi.test file in order to have a better visual order

--- client/src/services/test/productApi.test.js
+++ client/src/services/test/productApi.test.js
@@ -15,14 +15,12 @@ describe("Testing the productApi service", () => {
   it("should return all products when getAllProducts is called", async () => {
     const mockAllProducts = dataSet.productsData[0];
     mock.onGet("/products").reply(200, mockAllProducts);
-
     const actualProducts = await productApi.getAllProducts();
     expect(actualProducts).toEqual(mockAllProducts);
   });
 
   it("should return an error message when the getAllProducts call fails", async () => {
     mock.onGet("/products").reply(500, FETCH_PRODUCT_DATA_ERROR);
-
     const actualResponse = await productApi.getAllProducts();
     expect(actualResponse).toEqual(FETCH_PRODUCT_DATA_ERROR);
   });
@@ -30,15 +28,12 @@ describe("Testing the productApi service", () => {
   it("should return success message when addNewProduct is called", async () => {
     const productToAdd = dataSet.productsData[0][0];
     mock.onPost(`/products/`, productToAdd).reply(201, ADD_NEW_PRODUCT_SUCCESS);
-
     const actualProducts = await productApi.addNewProduct(productToAdd);
     expect(actualProducts).toEqual(ADD_NEW_PRODUCT_SUCCESS);
   });
 
   it("should return an error message when the addNewProduct call fails", async () => {
-    const productToAdd = dataSet.productsData[0][0];
     mock.onPost(`/products/`, {}).reply(500, ADD_NEW_PRODUCT_ERROR);
-
     const actualResponse = await productApi.addNewProduct();
     expect(actualResponse).toEqual(ADD_NEW_PRODUCT_ERROR);
   });
